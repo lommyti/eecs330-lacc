cc81489cf3e456e60a569c46a1146947d2945a15
lommyti <thomasli2020@u.northwestern.edu>
2018-02-16 11:50:31 -0600

    2% milk closes modal on click

--- main.js
+++ main.js
@@ -89,17 +89,6 @@ d3.tsv("https://s3-us-west-2.amazonaws.com/cbeas.misc/data.tsv", function(error,
 
 // ** Update data section (Called from the onclick)
 function updateData() {
-    
-    // Get the data again
-    d3.tsv("data-alt.tsv", function(error, data) {
-        data.forEach(function(d) {
-            d.date = parseDate(d.date);
-            d.close = +d.close;
-        });
-
-        // Scale the range of the data again 
-        x.domain(d3.extent(data, function(d) { return d.date; }));
-        y.domain([0, d3.max(data, function(d) { return d.close; })]);
 
         // Select the section we want to apply our changes to
         var svg = d3.select("body").transition();
@@ -114,9 +103,6 @@ function updateData() {
         svg.select(".y.axis") // change the y axis
             .duration(750)
             .call(yAxis);
-    
-    })
-    ;
 }
 
 
