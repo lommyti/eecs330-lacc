601e1bcc3d5f001fdd4d4c8d2eea76b5cb0bd11e
lommyti <thomasli2020@u.northwestern.edu>
2018-02-16 02:14:27 -0600

    line graph transform

--- main.js
+++ main.js
@@ -1,58 +1,122 @@
+var margin = {
+    top: 4,
+    right: 0,
+    bottom: 8,
+    left: 4
+},
+    width = 650 - margin.left - margin.right,
+    height = 240 - margin.top - margin.bottom;
 
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var parseDate = d3.time.format("%Y%m%d").parse;
 
-// parse the date / time
-var parseTime = d3.timeParse("%d-%b-%y");
+var x = d3.time.scale()
+.range([0, width]);
 
-// set the ranges
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+var y = d3.scale.linear()
+.range([height, 0]);
 
-// define the line
-var valueline = d3.line()
-.x(function(d) { return x(d.date); })
-.y(function(d) { return y(d.close); });
+var xAxis = d3.svg.axis()
+.scale(x)
+.orient("bottom");
 
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
+var yAxis = d3.svg.axis()
+.scale(y)
+.orient("left");
+
+var line = d3.svg.line()
+.interpolate("basis")
+.x(function(d) {
+    return x(d.date);
+})
+.y(function(d) {
+    return y(d.trend);
+});
+
+var svg = d3.select(".graph").append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
 .append("g")
-.attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-// Get the data
-d3.csv("data.csv", function(error, data) {
+d3.tsv("https://s3-us-west-2.amazonaws.com/cbeas.misc/data.tsv", function(error, data) {
     if (error) throw error;
 
-    // format the data
     data.forEach(function(d) {
-        d.date = parseTime(d.date);
-        d.close = +d.close;
+        d.date = parseDate(d.date);
+        d.trend = +d.trend;
     });
 
-    // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.close; })]);
+    x.domain([data[0].date, data[data.length - 1].date]);
+    y.domain(d3.extent(data, function(d) {
+        return d.trend;
+    }));
 
-    // Add the valueline path.
+    svg.append("linearGradient")
+        .attr("id", "temperature-gradient")
+        .attr("gradientUnits", "userSpaceOnUse")
+        .attr("x1", 0).attr("y1", y(0))
+        .attr("x2", 0).attr("y2", y(100))
+        .selectAll("stop")
+        .data([{
+            offset: "0%",
+            color: "#2D6785"
+        }, {
+            offset: "25%",
+            color: "#41A7E1"
+        },{
+            offset: "50%",
+            color: "orange"
+        }, {
+            offset: "75%",
+            color: "#EC9030"
+        }, {
+            offset: "100%",
+            color: "#EC5B30"
+        }])
+        .enter().append("stop")
+        .attr("offset", function(d) {
+        return d.offset;
+    })
+        .attr("stop-color", function(d) {
+        return d.color;
+    });
+
     svg.append("path")
-        .data([data])
+        .datum(data)
         .attr("class", "line")
-        .attr("d", valueline);
-
-    // Add the X Axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    // Add the Y Axis
-    svg.append("g")
-        .call(d3.axisLeft(y));
-
+        .attr("d", line);
 });
+
+// ** Update data section (Called from the onclick)
+function updateData() {
+    
+    // Get the data again
+    d3.tsv("data-alt.tsv", function(error, data) {
+        data.forEach(function(d) {
+            d.date = parseDate(d.date);
+            d.close = +d.close;
+        });
+
+        // Scale the range of the data again 
+        x.domain(d3.extent(data, function(d) { return d.date; }));
+        y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+        // Select the section we want to apply our changes to
+        var svg = d3.select("body").transition();
+
+        // Make the changes
+        svg.select(".line")
+            .duration(750)
+            .style("transform", "scaleY(.6)");
+        svg.select(".x.axis") // change the x axis
+            .duration(750)
+            .call(xAxis);
+        svg.select(".y.axis") // change the y axis
+            .duration(750)
+            .call(yAxis);
+    
+    })
+    ;
+}
+
+
