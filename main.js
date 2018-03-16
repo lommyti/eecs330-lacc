aa76a0ee694a3a4c93bc2b3deae9aaa32575466e
lommyti <thomasli2020@u.northwestern.edu>
2018-03-16 02:32:50 -0500

    done

--- main.js
+++ main.js
@@ -120,12 +120,6 @@ function changeSelection(id) {
     else{
         if (id.includes('star')) {
             x.style.color = 'rgb(255, 215, 0)';
-            console.log("this is what's happening");
-            var row = document.getElementById("favs").insertRow(1);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            cell1.innerHTML = "swag";
-            cell2.innerHTML = "fire";
         }
         else {
             x.style.color = 'red';
@@ -261,7 +255,8 @@ function mySearchFunction() {
 var lacFilter = false;
 var tenFilter = false;
 var fifteenFilter = false;
-
+var foodFilter = false;
+var drinkFilter = false;
 
 function searchLowLactose() {
     // Declare variables 
@@ -277,9 +272,16 @@ function searchLowLactose() {
         // Loop through all table rows, and hide those who don't match the search query
         for (i = 0; i < tr.length; i++) {
             td = tr[i].getElementsByTagName("td")[1];
+            td2 = tr[i].getElementsByTagName("td")[0];
             if (td) {
                 console.log(td.innerHTML);
                 tr[i].style.display = "";
+                if(foodFilter && td2.innerHTML=="1% Milk"){
+                    tr[i].style.display = "none";
+                }
+                else if(drinkFilter && td2.innerHTML != "1% Milk"){
+                    tr[i].style.display = "none";
+                }
                 } 
             else {
                 }
@@ -295,10 +297,17 @@ function searchLowLactose() {
         // Loop through all table rows, and hide those who don't match the search query
         for (i = 0; i < tr.length; i++) {
             td = tr[i].getElementsByTagName("td")[1];
+            td2 = tr[i].getElementsByTagName("td")[0];
             if (td) {
                 console.log(td.innerHTML);
                 if (filter.includes(td.innerHTML)) {
                     tr[i].style.display = "";
+                    if (drinkFilter && (td2.innerHTML == "Yoplait Yogurt" || td2.innerHTML == "Oikos Yogurt")) {
+                        tr[i].style.display = "none";
+                    }
+                    else if (foodFilter && td2.innerHTML == "1% Milk"){
+                        tr[i].style.display = "none";
+                    }
                 } else {
                     tr[i].style.display = "none";
                 }
@@ -311,7 +320,7 @@ function searchLowLactose() {
 function search10() {
     // Declare variables 
     var filter, table, tr, td, i;
-    filter = ['1mg', '2mg', '3mg', '4mg', '5mg', '6mg', '7mg', '8mg', '9mg', '10mg'];
+    filter = ['1mg', '2mg', '3mg', '4mg', '5mg', '6mg', '7mg', '8mg', '9mg'];
     table = document.getElementById("search-result");
     tr = table.getElementsByTagName("tr");
 
@@ -322,8 +331,15 @@ function search10() {
         // Loop through all table rows, and hide those who don't match the search query
         for (i = 0; i < tr.length; i++) {
             td = tr[i].getElementsByTagName("td")[1];
+            td2 = tr[i].getElementsByTagName("td")[0];
             if (td) {
                 tr[i].style.display = "";
+                if(td2.innerHTML != "1% Milk" && drinkFilter){
+                    tr[i].style.display = "none";
+                }
+                else if(td2.innerHTML == "1% Milk" && foodFilter){
+                    tr[i].style.display = "none";
+                }
             } 
         }
     }
@@ -336,10 +352,14 @@ function search10() {
         fifteenFilter = false;
         for (i = 0; i < tr.length; i++) {
             td = tr[i].getElementsByTagName("td")[1];
+            td2 = tr[i].getElementsByTagName("td")[0];
             if (td) {
                 console.log(td.innerHTML);
                 if (filter.includes(td.innerHTML)) {
                     tr[i].style.display = "";
+                    if(td2.innerHTML == "Oikos Yogurt" && drinkFilter){
+                        tr[i].style.display = "none";
+                    }
                 } else {
                     tr[i].style.display = "none";
                 }
@@ -363,8 +383,16 @@ function search15() {
         document.getElementById("fifteenFilter").style.background = "#46acc2";
         for (i = 0; i < tr.length; i++) {
             td = tr[i].getElementsByTagName("td")[1];
+            td2 = tr[i].getElementsByTagName("td")[0];
+
             if (td) {
                 tr[i].style.display = "";
+                if(foodFilter && td2.innerHTML == "1% Milk"){
+                    tr[i].style.display = "none";
+                }
+                else if(drinkFilter && td2.innerHTML != "1% Milk"){
+                    tr[i].style.display = "none";
+                }
             } 
         }
     }
@@ -377,10 +405,118 @@ function search15() {
         tenFilter = false;
         for (i = 0; i < tr.length; i++) {
             td = tr[i].getElementsByTagName("td")[1];
+            td2 = tr[i].getElementsByTagName("td")[0];
             if (td) {
                 console.log(td.innerHTML);
                 if (filter.includes(td.innerHTML)) {
                     tr[i].style.display = "";
+                    if (drinkFilter && (td2.innerHTML == "Yoplait Yogurt" || td2.innerHTML == "Oikos Yogurt")) {
+                        tr[i].style.display = "none";
+                    }
+                    else if (foodFilter && td2.innerHTML == "1% Milk"){
+                        tr[i].style.display = "none";
+                    }
+                } else {
+                    tr[i].style.display = "none";
+                }
+            } 
+        }
+    }
+}
+
+function searchFood(){
+    // Declare variables 
+    var filter, table, tr, td, i;
+    filter = ['Yogurt'];
+    table = document.getElementById("search-result");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+
+    if (foodFilter){
+        foodFilter = false;
+        document.getElementById("foodFilter").style.background = "#46acc2";
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                tr[i].style.display = "";
+                if((lacFilter||fifteenFilter||tenFilter) && (td.innerHTML == "Chobani Yogurt" || td.innerHTML == "Dannon Yogurt")){
+                    tr[i].style.display = "none";
+                }
+                if(tenFilter && td.innerHTML != "Oikos Yogurt"){
+                    tr[i].style.display = "none";
+                }
+            } 
+        }
+    }
+    else{
+        document.getElementById("foodFilter").style.background = "#ef9b2f";
+        document.getElementById("drinkFilter").style.background = "#46acc2";
+        foodFilter = true;
+        drinkFilter = false;
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                console.log(td.innerHTML);
+                if (td.innerHTML.includes(filter)) {
+                    tr[i].style.display = "";
+                    if((td.innerHTML=="Chobani Yogurt" || td.innerHTML=="Dannon Yogurt") && lacFilter){
+                        tr[i].style.display = "none";
+                    }
+                    else if((td.innerHTML=="Chobani Yogurt" || td.innerHTML=="Dannon Yogurt") && fifteenFilter){
+                        tr[i].style.display = "none";
+                    }
+                    else if((td.innerHTML!= "Oikos Yogurt") && tenFilter){
+                        tr[i].style.display = "none";
+                    }
+                } else {
+                    tr[i].style.display = "none";
+                }
+            } 
+        }
+    }
+}
+
+function searchDrink(){
+    // Declare variables 
+    var filter, table, tr, td, i;
+    filter = ['Milk'];
+    table = document.getElementById("search-result");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+
+    if (drinkFilter){
+        drinkFilter = false;
+        document.getElementById("drinkFilter").style.background = "#46acc2";
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                tr[i].style.display = "";
+                if((lacFilter||fifteenFilter) &&( td.innerHTML=="Chobani Yogurt" || td.innerHTML=="Dannon Yogurt")){
+                   tr[i].style.display = "none";
+
+                   } 
+                if(tenFilter && td.innerHTML!="Oikos Yogurt"){
+                    tr[i].style.display = "none";
+                }
+        }
+    }
+                   }
+    else{
+        document.getElementById("drinkFilter").style.background = "#ef9b2f";
+        document.getElementById("foodFilter").style.background = "#46acc2";
+        foodFilter = false;
+        drinkFilter = true;
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                console.log(td.innerHTML);
+                if (td.innerHTML.includes(filter)) {
+                    tr[i].style.display = "";
+                    if(tenFilter){
+                        tr[i].style.display = "none";
+                    }
                 } else {
                     tr[i].style.display = "none";
                 }
